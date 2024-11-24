91f0d6168b652bc1181882866ebf1034620fd8c0
phuongit1306 <phuongit1306@gmail.com>
2024-11-25 02:37:04 +0700

    Weapon Cooldown

--- Assets/Scripts/Player/ActiveWeapon.cs
+++ Assets/Scripts/Player/ActiveWeapon.cs
@@ -7,6 +7,7 @@ public class ActiveWeapon : Singleton<ActiveWeapon>
     public MonoBehaviour CurrentActiveWeapon { get; private set; }
 
     private PlayerControls playerControls;
+    private float timeBetweenAttacks;
 
     private bool attackButtonDown, isAttacking = false;
 
@@ -25,6 +26,8 @@ public class ActiveWeapon : Singleton<ActiveWeapon>
     {
         playerControls.Combat.Attack.started += _ => StartAttacking();
         playerControls.Combat.Attack.canceled += _ => StopAttacking();
+
+        AttackCooldown();
     }
 
     private void Update() {
@@ -33,14 +36,24 @@ public class ActiveWeapon : Singleton<ActiveWeapon>
 
     public void NewWeapon(MonoBehaviour newWeapon) {
         CurrentActiveWeapon = newWeapon;
+
+        AttackCooldown();
+        timeBetweenAttacks = (CurrentActiveWeapon as IWeapon).GetWeaponInfo().weaponCooldown;
     }
 
     public void WeaponNull() {
         CurrentActiveWeapon = null;
     }
 
-    public void ToggleIsAttacking(bool value) {
-        isAttacking = value;
+    private void AttackCooldown() {
+        isAttacking = true;
+        StopAllCoroutines();
+        StartCoroutine(TimeBetweenAttacksRoutine());
+    }
+
+    private IEnumerator TimeBetweenAttacksRoutine() {
+        yield return new WaitForSeconds(timeBetweenAttacks);
+        isAttacking = false;
     }
 
     private void StartAttacking()
@@ -55,7 +68,7 @@ public class ActiveWeapon : Singleton<ActiveWeapon>
 
     private void Attack() {
         if (attackButtonDown && !isAttacking) {
-            isAttacking = true;
+            AttackCooldown();
             (CurrentActiveWeapon as IWeapon).Attack();
         }
     }
