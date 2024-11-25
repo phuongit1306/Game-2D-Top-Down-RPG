ac4a979694ec0a570e321383d9a64694521fb3c4
phuongit1306 <phuongit1306@gmail.com>
2024-11-25 14:14:50 +0700

    Update Damage Arrow & Magic + Magic Laser

--- Assets/Scripts/UI/Staff.cs
+++ Assets/Scripts/UI/Staff.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Staff : MonoBehaviour, IWeapon
 {
     [SerializeField] private WeaponInfo weaponInfo;
+    [SerializeField] private GameObject magicLaser;
+    [SerializeField] private Transform magicLaserSpawnPoint;
+
+    private Animator myAnimator;
+
+    readonly int AttackHash = Animator.StringToHash("Attack");
+
+    private void Awake() {
+        myAnimator = GetComponent<Animator>();
+    }
 
     private void Update() {
         MouseFollowWithOffset();
@@ -12,7 +22,16 @@ public class Staff : MonoBehaviour, IWeapon
 
 
     public void Attack() {
-        Debug.Log("Staff Attack");
+        myAnimator.SetTrigger(AttackHash);
+    }
+
+    public void SpawnStaffProjectileAnimEvent() {
+        GameObject newLaser = Instantiate(magicLaser, magicLaserSpawnPoint.position, Quaternion.identity);
+    }
+
+    public WeaponInfo GetWeaponInfo()
+    {
+        return weaponInfo;
     }
 
     private void MouseFollowWithOffset()
@@ -30,10 +49,5 @@ public class Staff : MonoBehaviour, IWeapon
         {
             ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, 0, angle);
         }
-    }
-
-    public WeaponInfo GetWeaponInfo()
-    {
-        return weaponInfo;
     }
 }
